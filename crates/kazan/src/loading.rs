1bb0e0a720417d7cef082093401e531253d4bcdd
Joerg Wollenschlaeger <joerg.wollenschlaeger@gmail.com>
2026-03-07 20:28:02 +0900

    Test instance creation

--- crates/kazan/src/loading.rs
+++ crates/kazan/src/loading.rs
@@ -21,7 +21,7 @@ pub struct Entry {
 
 impl Entry {
     #[cfg(feature = "linked")]
-    pub fn linked() -> Result<Self, LoadingError> {
+    pub fn linked() -> Result<Self, MissingEntryPointError> {
         // Sound because we're linking to Vulkan, which provides a vkGetInstanceProcAddr that has
         // defined behavior in this use.
         unsafe {
@@ -62,11 +62,13 @@ impl Entry {
     pub unsafe fn load_from(path: impl AsRef<OsStr>) -> Result<Self, LoadingError> {
         let lib = unsafe { Library::new(path.as_ref()).map_err(LoadingError::LibraryLoadFailure)? };
 
-        let static_fn = unsafe { StaticFn::load(|name|
-            lib.get(name.to_bytes_with_nul())
-                .map(|symbol| *symbol)
-                .unwrap_or(None)
-        ) }?;
+        let static_fn = unsafe {
+            StaticFn::load(|name| {
+                lib.get(name.to_bytes_with_nul())
+                    .map(|symbol| *symbol)
+                    .unwrap_or(None)
+            })
+        }?;
 
         Ok(Self {
             _lib_guard: Some(lib),
@@ -74,7 +76,7 @@ impl Entry {
         })
     }
 
-    pub unsafe fn from_static_fn(static_fn: StaticFn) -> Result<Self, LoadingError> {
+    pub unsafe fn from_static_fn(static_fn: StaticFn) -> Result<Self, MissingEntryPointError> {
         let load_fn = move |name: &CStr| unsafe {
             mem::transmute((static_fn.get_instance_proc_addr)(
                 crate::vk::Instance::null(),
@@ -100,12 +102,12 @@ pub struct StaticFn {
 }
 
 impl StaticFn {
-    pub unsafe fn load<F>(mut f: F) -> Result<Self, LoadingError>
+    pub unsafe fn load<F>(mut f: F) -> Result<Self, MissingEntryPointError>
     where
         F: FnMut(&CStr) -> Option<crate::vk::PFN_vkVoidFunction>,
     {
         let get_instance_proc_addr = f(c"vkGetInstanceProcAddr")
-            .ok_or(LoadingError::MissingEntryPoint(MissingEntryPointError))?;
+            .ok_or(MissingEntryPointError)?;
 
         Ok(Self {
             get_instance_proc_addr: unsafe { mem::transmute(get_instance_proc_addr) },
@@ -171,6 +173,7 @@ mod loaded {
 #[cfg(feature = "loaded")]
 pub use self::loaded::*;
 
+#[cfg(feature = "loaded")]
 mod tests {
     use crate::vk;
 
@@ -192,20 +195,81 @@ mod tests {
         }
     }
 
+    pub const fn make_api_version(variant: u32, major: u32, minor: u32, patch: u32) -> u32 {
+        ((variant) << 29) | ((major) << 22) | ((minor) << 12) | (patch)
+    }
+
+    struct InstanceFn {
+        vk1_0: vk::vk1_0::InstanceFn,
+    }
+
     #[test]
     fn test_loaded() -> Result<(), TestError> {
         let entry = unsafe { Entry::load()? };
-        
-        if let Some(vk1_1) = entry.vk1_1 {
-            let api_version = unsafe { vk1_1.enumerate_instance_version()? };
-            println!("API version: {}", api_version);
+
+        let api_version = if let Some(vk1_1) = entry.vk1_1 {
+            unsafe { vk1_1.enumerate_instance_version()? }
+        } else {
+            make_api_version(0, 1, 0, 0)
+        };
+        println!("API version: {}", api_version);
+
+        let mut extensions = Vec::new();
+        unsafe {
+            entry
+                .vk1_0
+                .enumerate_instance_extension_properties(None, &mut extensions)?
+        };
+        for extension in extensions {
+            println!("Extension: {}", unsafe {
+                CStr::from_ptr(extension.extension_name.as_ptr()).to_string_lossy()
+            });
         }
 
-        let mut extensions = Vec::new() ;
-        unsafe { entry.vk1_0.enumerate_instance_extension_properties(None, &mut extensions)? };
-        for extension in extensions {
-            println!("Extension: {}", unsafe { CStr::from_ptr(extension.extension_name.as_ptr()).to_string_lossy() });
+        let application_info = vk::ApplicationInfo::default().api_version(api_version);
+
+        let instance_create_info =
+            vk::InstanceCreateInfo::default().application_info(&application_info);
+
+        let instance = unsafe { entry.vk1_0.create_instance(&instance_create_info, None)? };
+        println!("Instance: {:?}", instance);
+
+        let instance_fn = InstanceFn {
+            vk1_0: unsafe {
+                vk::vk1_0::InstanceFn::load(|name| {
+                    mem::transmute((entry.static_fn.get_instance_proc_addr)(
+                        instance,
+                        name.as_ptr(),
+                    ))
+                })
+                .map_err(|e| TestError::Loading(LoadingError::MissingEntryPoint(e)))?
+            },
+        };
+
+        unsafe {
+            let mut physical_devices = Vec::new();
+            instance_fn
+                .vk1_0
+                .enumerate_physical_devices(instance, &mut physical_devices)?;
+            
+            for physical_device in &physical_devices {
+                let mut extension_properties = Vec::new();
+                instance_fn.vk1_0.enumerate_device_extension_properties(
+                    *physical_device,
+                    None,
+                    &mut extension_properties,
+                )?;
+                for extension_property in &extension_properties {
+                    println!(
+                        "Extension property: {}",
+                        CStr::from_ptr(extension_property.extension_name.as_ptr())
+                            .to_string_lossy()
+                    );
+                }
+            }
         }
+
+        unsafe { instance_fn.vk1_0.destroy_instance(instance, None) };
 
         Ok(())
     }
